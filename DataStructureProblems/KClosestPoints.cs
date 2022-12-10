500a3b6e5bf0d13c2d07d7391bf78b8a85a02f75
Mumtazben Shaikh <mumtazben.shaikh@dreamlines.com>
2022-12-10 08:39:17 +0100

    Added comments

--- DataStructureProblems/KClosestPoints.cs
+++ DataStructureProblems/KClosestPoints.cs
@@ -41,7 +41,7 @@ namespace DataStructureProblems
             // sort the points in ascending order of their distance
             // from the origin
             var closestPoints = arr.OrderBy(p => Distance(p)).Take(k);
-
+            
             // print the first k closest points
             Console.WriteLine("The {0} closest points are:", k);
             foreach (var p in closestPoints)
@@ -51,10 +51,19 @@ namespace DataStructureProblems
     }
 }
 
-/*
+/* Explanation
  * This code first defines a Point structure to represent a point in 2D plane, and then defines a function Distance 
  * that calculates the distance of a point from the origin.
 
 In the Main function, we create an array of points, and then use the OrderBy and Take methods to sort the points in ascending order 
 of their distance from the origin, and take the first k points. Finally, we print the k closest points.
+
+The Take method is often used in combination with the OrderBy method, which sorts the elements in a sequence 
+in ascending order according to a specified key. 
+
+var closestPoints = arr.OrderBy(p => Distance(p)).Take(k);
+This code sorts the points in the arr array in ascending order of their distance from the origin,
+using the Distance function as the key, and then takes the first k points from the sorted array. 
+The resulting sequence of points can then be used to find the k closest points to the origin.
+
  */
